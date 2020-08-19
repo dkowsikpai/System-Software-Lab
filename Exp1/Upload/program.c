cf9792919c7a04c6bbf68a6be1b77a83180e4514
dkowsikpai <dkowsikpai@gmail.com>
2020-08-19 23:13:02 +0530

    Modified Exp1

--- Exp1/Upload/program.c
+++ Exp1/Upload/program.c
@@ -1,11 +1,26 @@
+/**
+ * Program: Implementation of scheduling Algorithm
+ * Author: Kowsik Nandagopan D, CSE, GEC Thrissur
+*/
+
+// Header File
 #include<stdio.h>
 #include<stdlib.h>
+// Setting Quantum for Round Robin
 #define QUANTUM 3
 
+// Global Variables: Number of processes n, at: Arrival Time
+// bt: Burst Time, tat: Turn Arround Time, wt: Wait Time, prio: Priority
 int n=0, *at, *bt, *tat, *wt, *prio;
+// Temp Variables
 int a, b , c, i = 0;
 
+// Print to file file pointer
+FILE *fout;
+
+// Re-read file: without quitting the program we can modify file
 void reread(){
+    // Reinitialization
     n = 0;
     at = NULL;
     bt = NULL;
@@ -13,18 +28,20 @@ void reread(){
     wt = NULL;
     prio = NULL;
     i = 0;
+    // Reading from the input.txt file
     FILE *fptr;
     if ((fptr = fopen("input.txt", "r")) == NULL) {
         printf("Error! opening file");
         exit(1);
     }
-
+    // Taking the count of number of processes
     while (!feof(fptr)){
         fscanf(fptr, "%d\t%d\t%d", &a, &b, &c);
         n++;
     }
     rewind(fptr);
 
+    // Allocating space for the dynamic array
     at = (int*) malloc(n * sizeof(int));
     bt = (int*) malloc(n * sizeof(int));
     tat = (int*) malloc(n * sizeof(int));
@@ -36,6 +53,7 @@ void reread(){
         exit(0);
     }
 
+    // Reading from file
     while (!feof(fptr)){
         fscanf(fptr, "%d\t%d\t%d", at+i, bt+i, prio+i);
         i++;        
@@ -43,7 +61,8 @@ void reread(){
     fclose(fptr);
 }
 
-void findBurstTimeFCFS(int n, int *at, int *bt, int *tat){
+// First in first served Turn Around Calculation
+void findTurnAroundFCFS(int n, int *at, int *bt, int *tat){
     int cum=0;
     for (int i=0; i < n; i++){
         *(tat+i) = cum - *(at+i) + *(bt + i);
@@ -51,10 +70,11 @@ void findBurstTimeFCFS(int n, int *at, int *bt, int *tat){
     }
 }
 
-void findBurstTimeSJF(int n, int *at, int *bt, int *tat){
-    int cum=0, count=0, t=0, min=1000, index=-1;
+// Shortest Job First Turn Around Calculation
+void findTurnAroundSJF(int n, int *at, int *bt, int *tat){
+    int cum=0, count=0, t=0, min=10000, index=-1;
     while(1) {
-        min=1000;
+        min=10000;
         index=-1;
         for (int i=0; i<n; i++){
             if (*(at+i) <= t && *(at+i)!=-1 && *(bt+i) < min) {
@@ -73,15 +93,13 @@ void findBurstTimeSJF(int n, int *at, int *bt, int *tat){
     }
 }
 
-void findBurstTimeRR(int n, int *at, int *bt, int *tat){
-
+// Round Robin Turn Around Calculation
+void findTurnAroundRR(int n, int *at, int *bt, int *tat){
     int *bt_temp = (int *)malloc(n*sizeof(int));
     int count=0, t=0;
-
     for (int i=0; i<n; i++){
         *(bt_temp+i) = *(bt+i);
     }
-
     while(1) {
         for(int i=0; i < n; i++){
             if (*(at+i) <= t && *(at+i) != -1) {
@@ -102,10 +120,11 @@ void findBurstTimeRR(int n, int *at, int *bt, int *tat){
     }
 }
 
-void findBurstTimePriority(int n, int *at, int *bt, int *tat, int *prio){
-    int cum=0, count=0, t=0, min=1000, index=-1;
+// Priority Sheduling Turn Around Calculation
+void findTurnAroundPriority(int n, int *at, int *bt, int *tat, int *prio){
+    int cum=0, count=0, t=0, min=10000, index=-1;
     while(1) {
-        min=1000;
+        min=10000;
         index=-1;
         for (int i=0; i<n; i++){
             if (*(at+i) <= t && *(at+i) != -1 && *(prio+i) < min){
@@ -123,55 +142,87 @@ void findBurstTimePriority(int n, int *at, int *bt, int *tat, int *prio){
     }
 }
 
+// Waiting Time calculation in all the cases
 void findWaitingTime(int n, int *bt, int *tat, int *wt){
     for (int i=0; i < n; i++){
         *(wt+i) = *(tat+i) - *(bt+i);
     }
 }
 
+// Displaying output of funtions and printing to the file
 void display(){
     printf("Turn Arount Time\tWaiting Time\n");
+    fprintf(fout, "Turn Arount Time\tWaiting Time\n");
     for (int i=0; i < n; i++){
         // printf("%d\n", *(tat + i));
         printf("%d\t\t\t%d\n", *(tat + i), *(wt + i));
+        fprintf(fout, "%d\t\t\t%d\n", *(tat + i), *(wt + i));
     }
 }
 
 void main(){
     int menu=5, flag=1;
+    // Printing Output to output.txt
+    fout = fopen("output.txt", "w");
+    if (fout == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    // Reading and printing the contents of input.txt to console and output.txt
+    printf("Input file contents:\n");
+    fprintf(fout, "Input file contents:\n");
+    FILE *fptr;
+    if ((fptr = fopen("input.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
+    printf("Arrival Time\tBurst Time\tPriority\n");
+    fprintf(fout, "Arrival Time\tBurst Time\tPriority\n");
+    while (!feof(fptr)){
+        fscanf(fptr, "%d\t%d\t%d", &a, &b, &c);
+        printf("%d\t\t%d\t\t%d\n", a, b, c);
+        fprintf(fout, "%d\t\t\t\t%d\t\t\t\t%d\n", a, b, c);
+    }
+    
+    // Menu Driver
     while (flag){
-        printf("Menu\n1.FCFS\n2.SJF\n3.RR\n4.Priority\n5.Exit\nSelect:");
+        printf("-----------------Menu----------------\n1.FCFS\n2.SJF\n3.RR\n4.Priority\n5.Exit\nSelect:");
         scanf("%d", &menu);
         switch (menu)
         {
-        case 1:
+        case 1: 
             printf("First-Come-First-Serverd\n");
+            fprintf(fout, "First-Come-First-Serverd\n");
             reread();
-            findBurstTimeFCFS(n, at, bt, tat);
+            findTurnAroundFCFS(n, at, bt, tat);
             findWaitingTime(n, bt, tat, wt);
             display();
             break;
 
         case 2:
             printf("Shortest-Job-First\n");
+            fprintf(fout, "Shortest-Job-First\n");
             reread();
-            findBurstTimeSJF(n, at, bt, tat);
+            findTurnAroundSJF(n, at, bt, tat);
             findWaitingTime(n, bt, tat, wt);
             display();
             break;
 
         case 3:
             printf("Round-Robin\n");
+            fprintf(fout, "Round-Robin\n");
             reread();
-            findBurstTimeRR(n, at, bt, tat);
+            findTurnAroundRR(n, at, bt, tat);
             findWaitingTime(n, bt, tat, wt);
             display();
             break;
 
         case 4:
             printf("Priority\n");
+            fprintf(fout, "Priority\n");
             reread();
-            findBurstTimePriority(n, at, bt, tat, prio);
+            findTurnAroundPriority(n, at, bt, tat, prio);
             findWaitingTime(n, bt, tat, wt);
             display();
             break;
@@ -182,7 +233,9 @@ void main(){
 
         default:
             printf("Invalid Menu\n");
+            fprintf(fout, "Invalid Menu\n");
             break;
         }
     }
+    fclose(fout);
 }
